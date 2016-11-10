65d6fffddb717a7156fe44b703e8bc83e5e644b6
Chuck Verret <chuck@chuckverret.com>
2016-11-09 20:23:26 -0600

    add grid shstem and style examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,12 +1,20 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-    <h3>Examples</h3>
-    <p>Welcome to example page.</p>
+    <h1 className="text-center">Examples</h1>
+    <p>Here are a few example locations to try out.</p>
+    <ul>
+      <li>
+        <Link to='/?location=Houston'>Houston, TX</Link>
+      </li>
+      <li>
+        <Link to='/?location=Austin'>Austin, TX</Link>
+      </li>
+    </ul>
     </div>
-
   );
 };
 
